c37c41781425949f6db82e4ef3a4af43beafb22d
Michael R <tekmedias@gmail.com>
2020-11-18 12:08:46 +0100

    :rocket: add fileselector / import os

--- streamlit/file.py
+++ streamlit/file.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import os
 
-st.title('Explorigin Data with Streamlit')
+st.title('Exploring Data with Streamlit')
 
-# DATE_COLUMN = 'date/time'
-# DATA_URL = ('https://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 DATA_URL = 'data/Pokemon.csv'
 
+# set cache request
 @st.cache
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
@@ -16,8 +16,22 @@ def load_data(nrows):
     # data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
+# function for file_selector
+def file_selector(folder_path='./data'):
+    filenames = os.listdir(folder_path)
+    selected_filename = st.selectbox("Select a dataset",filenames)
+    return os.path.join(folder_path,selected_filename)
+
+filename = file_selector()
+st.info("Dataset selected {}".format(filename))
+
+# Read Data
+df = pd.read_csv(filename)
+
+# loading the dataset
 data_load_state = st.text('Loading data...')
 data = load_data(10000)
+# success
 data_load_state.text("Done! (using st.cache)")
 
 if st.checkbox('Show raw data'):
@@ -25,7 +39,7 @@ if st.checkbox('Show raw data'):
     st.write(data)
 
 
-st.subheader('Test subheader')
+st.subheader('Data\'s columns')
 st.write(data.columns.tolist())
 
 # hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
