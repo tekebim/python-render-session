2ae9b99ae985b1ca29d73a97e061fe72c8ddc992
Michael R <tekmedias@gmail.com>
2020-11-18 11:30:14 +0100

    :zap: load custom dataset and display row datas columns

--- streamlit/file.py
+++ streamlit/file.py
@@ -2,18 +2,18 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Learning')
+st.title('Explorigin Data with Streamlit')
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+# DATE_COLUMN = 'date/time'
+# DATA_URL = ('https://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+DATA_URL = 'data/Pokemon.csv'
 
 @st.cache
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    # data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
 data_load_state = st.text('Loading data...')
@@ -26,5 +26,8 @@ if st.checkbox('Show raw data'):
 
 
 st.subheader('Test subheader')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
+st.write(data.columns.tolist())
+
+# hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+# st.bar_chart(hist_values)
+# st.write(data.columns.tolist())
