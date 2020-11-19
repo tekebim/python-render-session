c77298069d973f3fde09a06478250dd1ad18cf54
Michael R <tekmedias@gmail.com>
2020-11-19 09:58:37 +0100

    :rocket: Add readme / template / quickstart routes

--- flask/app.py
+++ flask/app.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def index():
- return "hello world!"
+    return "hello DC"
+
+@app.route("/hello", methods=['GET'])
+def hello_name():
+    return render_template('hello.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
