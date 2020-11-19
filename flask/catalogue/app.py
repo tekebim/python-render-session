ae83ec9ae6dbf61e3bb11a84832e22d2e9199bd4
Michael R <tekmedias@gmail.com>
2020-11-19 11:43:46 +0100

    :zap: correct endpoints

--- flask/catalogue/app.py
+++ flask/catalogue/app.py
@@ -12,26 +12,30 @@ booklist = [
 	}
 ]
 
+# default route index
 @app.route('/')
 def index():
     return 'hello my app'
 
+# endpoint all books
 @app.route('/api/books', methods=['GET'])
 def get_books():
     return jsonify(
         books = booklist
     )
 
+# endpoint id single book
 @app.route('/api/books/<int:book_id>', methods=['GET'])
 def get_book_id(book_id):
     for book in booklist :
         if book['id'] == int(book_id):
             return book
 
+# endpoint title single book
 @app.route('/api/books/<string:book_title>', methods=['GET'])
 def get_book_title(book_title):
     for book in booklist :
-        if book['title'] == book_title:
+        if book['titre'] == str(book_title):
             return book
 
 if __name__ == '__main__':
