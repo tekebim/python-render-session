13108f65c9b3c4f740a2fe5820a50dc94a62de17
Michael R <tekmedias@gmail.com>
2020-11-19 10:12:25 +0100

    :rocket: Add default template for route hello with param name

--- flask/app.py
+++ flask/app.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "hello DC"
+    return 'hello DC'
 
-@app.route("/hello", methods=['GET'])
-def hello_name():
-    return render_template('hello.html')
+@app.route('/about')
+def about():
+    return 'The about page'
+
+@app.route('/ma_route/')
+@app.route('/ma_route/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
